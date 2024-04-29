d092c6430d24695975d0206f88ad5256f93f78a0
Francisco Canela <fcanela.dev@gmail.com>
2024-04-29 10:33:20 +0000

    build: removes dotenv leftovers

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
