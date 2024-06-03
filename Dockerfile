f894a7db00d192fb0378072fb60faad084cdb0a0
Francisco Canela <fcanela.dev@gmail.com>
2024-06-03 07:47:14 +0000

    feat(docker): waits for external services to be ready using waitdeps

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM node:20.12.1-alpine
 
-USER node
+ENV WAITDEPS_VERSION 0.0.1
 
+RUN apk update --no-cache \
+  && apk add --no-cache wget openssl \
+  && wget -nv -O - https://github.com/fcanela/waitdeps/releases/download/$WAITDEPS_VERSION/waitdeps-$WAITDEPS_VERSION-linux-amd64.tar.gz | tar xzf - -C /usr/local/bin \
+  && apk del wget
+
+USER node
 RUN mkdir -p /home/node/app
 
 WORKDIR /home/node/app
@@ -10,6 +16,7 @@ COPY --chown=node package.json yarn.lock ./
 RUN yarn && yarn cache clean
 
 COPY --chown=node . .
+
 RUN yarn build
 
 ENTRYPOINT ["/bin/sh", "bin/start.sh"]
--- bin/start.sh
+++ bin/start.sh
@@ -5,8 +5,20 @@ echo "Validating environment variables..."
 node dist/utils/env.js
 
 echo "Waiting for dependencies to become ready..."
-# TODO: Perform this properly
-sleep 8
+WAITDEPS_CMD="waitdeps wait --timeout 30s --tcp-connect $DB_URI --tcp-connect $MAINNET_RPC_URL --tcp-connect $SNAPSHOT_GRAPHQL_URL"
+if [ -n "$SLACK_WEBHOOK" ]; then
+	WAITDEPS_CMD="$WAITDEPS_CMD --tcp-connect $SLACK_WEBHOOK"
+fi
+
+if [ -n "$SMTP_HOST" ]; then
+	if [ -n "$SMTP_PORT" ]; then
+		WAITDEPS_CMD="$WAITDEPS_CMD --tcp-connect $SMTP_HOST:$SMTP_PORT"
+	else
+		WAITDEPS_CMD="$WAITDEPS_CMD --tcp-connect $SMTP_HOST:465"
+	fi
+fi
+
+$WAITDEPS_CMD
 
 echo "Ensuring database schema is up to date..."
 yarn drizzle:migrate
