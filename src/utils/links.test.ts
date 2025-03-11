83468e54303184203fbca46b097b1e7cc7564af4
Francisco Canela <fcanela.dev@gmail.com>
2025-03-11 03:25:23 +0200

    feat: makes links flexible to support Base network

---
+++ src/utils/links.test.ts
@@ -0,0 +1,41 @@
+import { ProposalNotification } from "../notify";
+import { getBlockExplorerLinkForTx, getRealityQuestionLink, interpolateUrlTemplate } from "./links";
+import { expect } from "./tests-setup";
+
+describe("interpolateUrlTemplate", () => {
+  it("should substitute the requested fields", () => {
+    const template = "http://foo.com/{{value1}}?search={{value2}}";
+    const result = interpolateUrlTemplate(template, {
+      value1: "a",
+      value2: "b",
+    });
+    expect(result).to.eql("http://foo.com/a?search=b");
+  });
+});
+
+describe("getRealityQuestionLink", () => {
+  it("should generate a link using the oracle address and questionId", () => {
+    const oracleAddress = "x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef";
+    const questionId = "123";
+    const notification = {
+      space: {
+        oracleAddress,
+      },
+      event: {
+        questionId,
+      },
+    } as any as ProposalNotification;
+    const template = "http://test.com/{{oracleAddress}}/questions/{{questionId}}";
+    const result = getRealityQuestionLink(notification, template);
+    expect(result).to.eql(`http://test.com/${oracleAddress}/questions/${questionId}`);
+  });
+});
+
+describe("getBlockExplorerLinkForTx", () => {
+  it("should generate a link using the txHash", () => {
+    const txHash = "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef";
+    const template = "http://test.com/tx/{{txHash}}";
+    const result = getBlockExplorerLinkForTx(txHash, template);
+    expect(result).to.eql(`http://test.com/tx/${txHash}`);
+  });
+});
--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,5 +1,15 @@
+import type { Address } from "viem";
 import { AnswerNotification, ProposalNotification } from "../notify";
+import { env } from "./env";
 
+/**
+ * Given a notification, generates a link to the Snapshot propoasal page.
+ *
+ * @param notification - The notification object related to the triggering event
+ *
+ * @example
+ * const link = getSnapshotProposalLink(notification);
+ */
 export const getSnapshotProposalLink = (notification: ProposalNotification | AnswerNotification): string => {
   const {
     space: { ens },
@@ -9,10 +19,54 @@ export const getSnapshotProposalLink = (notification: ProposalNotification | Ans
   return `https://snapshot.org/#/${ens}/proposal/${snapshotId}`;
 };
 
-export const getRealityQuestionLink = (notification: ProposalNotification | AnswerNotification): string => {
+/**
+ * Interpolate a string representing a link with the given fields.
+ *
+ * @param template - The string template with placeholders for the fields using basic interpolation Handlebar syntax
+ * @param fields - The fields to be interpolated
+ *
+ * @example
+ * const link = interpolateUrlTemplate('http://example.com/{{foo}}', { foo: 'bar' }) // results in 'http://example.com/bar'
+ *
+ */
+export const interpolateUrlTemplate = (template: string, fields: Record<string, string>): string => {
+  return Object.entries(fields).reduce((acc, [key, value]) => {
+    return acc.replace(`{{${key}}}`, value);
+  }, template);
+};
+
+/**
+ * Given a notification, generates a link to the Reality question page.
+ *
+ * @param notification - The notification object related to the triggering event
+ * @param template - An string with handlebar-like syntax with the link structure
+ *
+ * @example
+ * const link = getRealityQuestionLink(notification);
+ */
+export const getRealityQuestionLink = (
+  notification: ProposalNotification | AnswerNotification,
+  template: string = env.REALITY_LINK_TEMPLATE,
+): string => {
   const {
     space: { oracleAddress },
     event: { questionId },
   } = notification;
-  return `https://reality.eth.limo/app/#!/question/${oracleAddress}-${questionId}/token/ETH`;
+  return interpolateUrlTemplate(template, { oracleAddress, questionId });
+};
+
+/**
+ * Given a transaction hash, generates a link to the block explorer (for example, Etherscan)
+ *
+ * @param txHash - The transaction hash
+ * @param template - An string with handlebar-like syntax with the link structure
+ *
+ * @example
+ * const link = getBlockExplorerLinkForTx('0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef');
+ */
+export const getBlockExplorerLinkForTx = (
+  txHash: Address,
+  template: string = env.BLOCK_EXPLORER_TX_LINK_TEMPLATE,
+): string => {
+  return interpolateUrlTemplate(template, { txHash });
 };
